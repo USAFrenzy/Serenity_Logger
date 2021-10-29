8246680b26f256de1e4533f3c0315a29a3c274ae
Ryan McCullough <mccullough.ryan123@gmail.com>
2021-10-28 21:22:01 -0400

    update

--- Sandbox/src/Experimental_SB/Experimental_Sandbox.cpp
+++ Sandbox/src/Experimental_SB/Experimental_Sandbox.cpp
@@ -83,11 +83,48 @@ enum class message_time_mode
 	local,
 	utc
 };
+static const std::array<const char *, 7> short_weekdays = { "Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat" };
 
-static std::array<const char *, 7>  weekdays = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
-static std::array<const char *, 12> Months   = { "January", "February", "March",     "April",   "May",      "Jun",
-                                               "July",    "August",   "September", "October", "November", "December" };
+static const std::array<const char *, 7> long_weekdays = { "Sunday",   "Monday", "Tuesday", "Wednesday",
+							   "Thursday", "Friday", "Saturday" };
 
+static const std::array<const char *, 12> short_months = { "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+							   "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+
+static const std::array<const char *, 12> long_months = { "January", "February", "March",     "April",   "May",      "June",
+							  "July",    "August",   "September", "October", "November", "December" };
+
+// TODO: Filter what strftime flags I decided to use here and see how i can get some to overlap one another
+/************************************************************************************************************
+								custom flags
+					************************************
+	- %N (Name)			- %L (Full Message Level)		- %l (Short Message Level)
+
+							The rest are strftime equivalents
+						******************************************
+	- %a (Abbrev Weekday Name)		- %d (Day Of Month)			- %T (HH:MM:SS Time format)
+	- %A (Full Weekday Name)		- %D (MM/DD/YY Date)		- %w (weekday as decimal 0-6)
+	- %b (Abbrev Month Name)		- %F (YYYY-MM-DD Date)		- %Y (Year XXXX)
+	- %B (Full Month Name)			- %H (24hr Hour format)		- %z (UTC offset)
+	- %M (Minute)					- %C (year XX Format)		- %S (Seconds)
+************************************************************************************************************/
+static const std::array<const char *, 16> flags = { "%N", "%a", "%A", "%b", "%B", "%C", "%d", "%D",
+						    "%F", "%H", "%M", "%S", "%T", "%w", "%Y", "%z" };
+
+struct Cached_Date_Time
+{
+	const char *long_weekday;  // String representation mapped from DayString()
+	const char *short_weekday;
+	int         hour { 0 };
+	int         min { 0 };
+	int         sec { 0 };
+	int         long_year { 0 };  // XXXX Year representation mapped from GetCurrentYear();
+	int         short_year { 0 };
+	const char *long_month;  // String representation mapped from MonthString()
+	const char *short_month;
+	int         day { 0 };
+	bool        initialized { false };
+};
 
 class Message_Time
 {
@@ -96,31 +133,80 @@ class Message_Time
 	{
 		UpdateTimeInfo( mode );
 	}
-
-	void UpdateTimeInfo( message_time_mode mode )
+	const char *WeekDayString( int weekdayIndex, bool shortened = false )
 	{
-		time = std::chrono::system_clock::to_time_t( std::chrono::system_clock::now( ) );
-		if( mode == message_time_mode::local ) {
-			t_struct = std::localtime( &time );
+		if( !shortened ) {
+			return long_weekdays.at( weekdayIndex );
 		}
 		else {
-			t_struct = std::gmtime( &time );
+			return short_weekdays.at( weekdayIndex );
+		}
+	}
+	int GetCurrentYear( int yearOffset, bool shortened = false )
+	{
+		if( !shortened ) {
+			return 1900 + yearOffset;  // XXXX Format
+		}
+		else {
+			return std::floor( yearOffset / 100 );  // XX Format Same as %C strftime
+		}
+	}
+	const char *MonthString( int monthIndex, bool shortened = false )
+	{
+		if( !shortened ) {
+			return long_months.at( monthIndex );
+		}
+		else {
+			return short_months.at( monthIndex );
 		}
 	}
 
-	//  Pretty Much Putting This Here For The Time Being So I Can Sit On How I Want To Implement A Formatter
-	/**********************************************************************************************************
-	***********************************************************************************************************
-		auto        weekday     = weekdays[ t_struct->tm_wday ];
-		auto        hour        = t_struct->tm_hour;
-		auto        min         = t_struct->tm_min;
-		auto        sec         = t_struct->tm_sec;
-		auto        year        = ( 1900 + t_struct->tm_year );
-		auto        month       = Months[ t_struct->tm_mon ];
-		auto day = t_struct->tm_mday;
-		return std::format( "{} {}/{}/{} {}:{}:{}", weekday, month, day, year, hour, min, sec );
-		***********************************************************************************************************
-		**********************************************************************************************************/
+	void InitializeCache( std::tm *t )
+	{
+		m_cache.long_year     = GetCurrentYear( t->tm_year );
+		m_cache.short_year    = GetCurrentYear( t->tm_year, true );
+		m_cache.long_month    = MonthString( t->tm_mon );
+		m_cache.short_month   = MonthString( t->tm_mon, true );
+		m_cache.long_weekday  = WeekDayString( t->tm_wday );
+		m_cache.short_weekday = WeekDayString( t->tm_wday, true );
+		m_cache.day           = t->tm_mday;
+		m_cache.hour          = t->tm_hour;
+		m_cache.min           = t->tm_min;
+		m_cache.sec           = t->tm_sec;
+		m_cache.initialized   = true;
+	}
+
+	void UpdateTimeInfo( message_time_mode mode )
+	{
+		m_time = std::chrono::system_clock::to_time_t( std::chrono::system_clock::now( ) );
+		// For both modes, check if the cache has been initialized, if so, then just update time variables. If not, then
+		// populate t_struct with time variables and initialize cache
+		if( mode == message_time_mode::local ) {
+			if( m_cache.initialized ) {
+				auto tmp          = std::localtime( &m_time );
+				t_struct->tm_hour = std::move( tmp->tm_hour );
+				t_struct->tm_min  = std::move( tmp->tm_min );
+				t_struct->tm_sec  = std::move( tmp->tm_sec );
+			}
+			else {
+				t_struct = std::localtime( &m_time );
+				InitializeCache( t_struct );
+			}
+		}  // mode == local
+		else {
+			if( m_cache.initialized ) {
+				auto tmp          = std::gmtime( &m_time );
+				t_struct->tm_hour = std::move( tmp->tm_hour );
+				t_struct->tm_min  = std::move( tmp->tm_min );
+				t_struct->tm_sec  = std::move( tmp->tm_sec );
+			}
+			else {
+				t_struct = std::gmtime( &m_time );
+				InitializeCache( t_struct );
+			}
+		}  // mode == utc
+	}
+
 	std::string FormatTime( std::string_view fmt, message_time_mode mode )
 	{
 		UpdateTimeInfo( mode );
@@ -136,15 +222,17 @@ class Message_Time
 
       private:
 	message_time_mode m_Mode;
-	std::time_t       time;
+	std::time_t       m_time;
 	std::tm *         t_struct = { };
+	Cached_Date_Time  m_cache  = { };
 };
 
 // This Might Also NOT Be The Way To Aid Formatting.. This Is Just An Idea
 // *************************************************************************
 _Enum_is_bitflag_ enum class Flags : uint8_t {
 	none = 0,       // no flags set
-	time = 1 << 0,  // Mapping To %H:%M:%S Time Format (Honestly Would Like A Way To Parallel strftime Substitution For This)
+	time = 1 << 0,  // Mapping To %H:%M:%S Time Format (Honestly Would Like A Way To Parallel strftime Substitution For
+			// This)
 	name = 1 << 1,  // Mapping To Logger Name
 	a    = 1 << 2,  // placeholder
 	b    = 1 << 3,  // placeholder
@@ -167,7 +255,7 @@ class Msg_Fmt_Flags
 	Msg_Fmt_Flags( )
 	{
 		flagHasher = {
-		  { "", Flags::none },
+		  { "%0", Flags::none },
 		  { "%T", Flags::time },
 		  { "%N", Flags::name }, /* From What I Can Tell, %N isn't Used In Any Formatters So Far */
 		};
@@ -180,22 +268,7 @@ class Msg_Fmt_Flags
 class Message_Pattern
 {
       public:
-	Message_Pattern( ) { }                           // what to set if no pattern passed in
 	Message_Pattern( std::string_view pattern ) { }  // use user's pattern
-
-	void SetMsgPattern( std::string_view pattern )
-	{
-		std::basic_format_parse_context fmt { pattern };
-		// I honestly have no idea about the std::formatter usage yet..
-		/*
-			Initial thoughts are to just roll a manual parser that checks for certain flags (yet to be implemented),
-			substitute those explicit values in for those flags in a string or buffer, then format the values based on the
-			pattern.
-			EDIT: If I flesh out the Flags enum and Msg_Fmt_Flags class, I could possibly use these to separately format
-		   and store each format unit (time, logger, date, message, color if console, etc) into a message pattern string that
-		   would be passed to a print or log type function
-		*/
-	}
 
 	std::string GetTimeFmt( )
 	{
@@ -203,16 +276,15 @@ class Message_Pattern
 	}
 
       private:
-	// currently forcing
-	std::string time_format = "%H:%M:%S";
-	//! NOTE: probably wrong!!!
-	std::formatter<std::chrono::seconds> timeStampFormatter;
+	// Weekday Day Month Year HH::MM::SS -UTC Offset
+	std::string time_format = "%a %Od %b %Oy %T%Ez";
 };
 
 class Message_Info
 {
       public:
-	Message_Info( ) = default;
+	Message_Info( ) : msgLevel( LoggerLevel::trace ), formats( "%T %N:" ), msg( ), msgTime( message_time_mode::local ) { }
+
 	Message_Info( LoggerLevel messageLevel, Message_Pattern formatPattern, std::string_view message, Message_Time messageTime )
 	  : msgLevel( messageLevel ), formats( formatPattern ), msg( message ), msgTime( messageTime )
 	{
@@ -228,16 +300,11 @@ class Message_Info
 		return msgLevel;
 	}
 
-	Message_Pattern GetFormatPatterns( )
-	{
-		return formats;
-	}
-
       private:
-	LoggerLevel      msgLevel { LoggerLevel::trace };
+	LoggerLevel      msgLevel;
+	Message_Pattern  formats;  // not currently used/implemented
 	std::string_view msg;
-	Message_Pattern  formats { };
-	Message_Time     msgTime { message_time_mode::local };
+	Message_Time     msgTime;
 };
 
 // clang-format off
@@ -274,13 +341,12 @@ class ColorConsole
 
 	void SetMsgColor( LoggerLevel level, const char *color )
 	{
-		msgLevelColors.at( level ) = color;
-		msgLevelColors[ level ]    = color;
+		msgLevelColors.at( level ) = std::move( color );
 	}
 
 	const char *GetMsgColor( LoggerLevel level )
 	{
-		return msgLevelColors[ level ];
+		return msgLevelColors.at( level );
 	}
 
 	bool ColorizeOutput( bool colorize )
@@ -295,9 +361,6 @@ class ColorConsole
 
 	template <typename... Args> void PrintMessage( LoggerLevel level, const std::string_view msg, Args &&...args )
 	{
-#ifdef INSTRUMENTATION_ENABLED
-		timer.StopWatch_Reset( );
-#endif  // INSTRUMENTATION_ENABLED
 		const char *msgColor;
 		if( coloredOutput ) {
 			msgColor = GetMsgColor( level );
@@ -308,10 +371,6 @@ class ColorConsole
 		std::cout << msgColor << MsgLevelToIcon( level ) << " " << message.GetMsgTime( ) << " "
 			  << "[" << loggerName << "]:"
 			  << " " << std::format( msg, std::forward<Args>( args )... ) << Reset( ) << "\n";
-#ifdef INSTRUMENTATION_ENABLED
-		timer.StopWatch_Stop( );
-		std::cout << "Elapsed In: " << timer.Elapsed_In( serenity::se_utils::time_mode::us ) << "us\n";
-#endif  // INSTRUMENTATION_ENABLED
 	}
 
 	// Not Apart Of This Class - Just Here For Testing (Part Of Logger Class)
@@ -366,18 +425,11 @@ class ColorConsole
 	}
 
       private:
-	Message_Info     message = { };
-	LoggerLevel      flush_level;
-	std::string_view loggerName = "Default_Console";
-	bool             coloredOutput { true };
-	// This Setup influenced by the hasher that spdlog uses to set colors for msg
-	// lvl
+	std::string_view                              loggerName = "Default_Console";
+	Message_Info                                  message    = { };
+	bool                                          coloredOutput { true };
 	std::unordered_map<LoggerLevel, const char *> msgLevelColors;
-	// for internal instrumentation
-#ifdef INSTRUMENTATION_ENABLED
-	serenity::se_utils::Instrumentator timer;
-#endif  // INSTRUMENTAION_ENABLED
-};      // struct ColorConsole
+};  // class ColorConsole
 
 int main( )
 {
@@ -390,9 +442,9 @@ int main( )
 	Instrumentator macroTester;
 	/*
 		First Iteration Takes Waaaaayyyy Too Long..
-		My guess so far, without full instrumentation, is that it has to do with the initial setup of the local time zone data?
-		I'm not even 50% sure if that's the case, but definitely want to flesh out the message details before mucking with
-		timing optimizations. Good to know there's a slight optimization issue this early on though I guess.
+		My guess so far, without full instrumentation, is that it has to do with the initial setup of the local time
+	   zone data? I'm not even 50% sure if that's the case, but definitely want to flesh out the message details before
+	   mucking with timing optimizations. Good to know there's a slight optimization issue this early on though I guess.
 	*/
 	macroTester.StopWatch_Reset( );
 #endif  // INSTRUMENTATION_ENABLED
@@ -416,11 +468,11 @@ int main( )
 	std::cout << Tag::Yellow(
 	  "\n\n***************************************************************\n******************** Instrumentation Data: "
 	  "********************\n***************************************************************\n" );
-	std::cout << Tag::Bright_Yellow( "Total Elapsed Time:\n" ) << Tag::Bright_Cyan( "\t- In Microseconds:\t" )
+	std::cout << Tag::Bright_Yellow( "Total Elapsed Time:\n" ) << Tag::Bright_Cyan( "\t- In Microseconds:\t\t" )
 		  << Tag::Bright_Green( std::to_string( macroTester.Elapsed_In( time_mode::us ) ) + " us\n" )
-		  << Tag::Bright_Cyan( "\t- In Milliseconds:\t" )
+		  << Tag::Bright_Cyan( "\t- In Milliseconds:\t\t" )
 		  << Tag::Bright_Green( std::to_string( macroTester.Elapsed_In( time_mode::ms ) ) + " ms\n" )
-		  << Tag::Bright_Cyan( "\t- In Seconds:\t\t" )
+		  << Tag::Bright_Cyan( "\t- In Seconds:\t\t\t" )
 		  << Tag::Bright_Green( std::to_string( macroTester.Elapsed_In( time_mode::sec ) ) + " s\n" );
 #endif  // INSTRUMENTATION_ENABLED
 
@@ -428,16 +480,23 @@ int main( )
 	#if ALLOC_TEST
 	// setting up for the current usage and allocations so no variation in conversions
 	auto mem_used = macroTester.mem_tracker.Memory_Usage( );
-	std::cout << Tag::Bright_Yellow( "Total Memory Used:\n" ) << Tag::Bright_Cyan( "\t- In Bytes:\t\t" )
-		  << Tag::Bright_Green( "[ " + std::to_string( mem_used ) + " bytes]\n" ) << Tag::Bright_Cyan( "\t- In Kilobytes:\t\t" )
+	std::cout << Tag::Bright_Yellow( "Total Memory Used:\n" ) << Tag::Bright_Cyan( "\t- In Bytes:\t\t\t" )
+		  << Tag::Bright_Green( "[ " + std::to_string( mem_used ) + " bytes]\n" )
+		  << Tag::Bright_Cyan( "\t- In Kilobytes:\t\t\t" )
 		  << Tag::Bright_Green( "[ " + std::to_string( mem_used / 1000.0 ) + " KB]\n" );
 	#endif  // ALLOC_TEST
 #endif          // INSTRUMENTATION_ENABLED
 
 #ifdef INSTRUMENTATION_ENABLED
-	// Currently 128 bytes
-	std::cout << Tag::Bright_Yellow( "Size of Message_Info Struct:\t" )
+	// Currently 144 bytes
+	std::cout << Tag::Bright_Yellow( "Size of Message_Info Struct:\t\t" )
 		  << Tag::Bright_Green( "[ " + std::to_string( sizeof( Message_Info ) ) + " bytes]\n" );
+	// Currentyly 64 bytes
+	std::cout << Tag::Bright_Yellow( "Size of Cached_Date_Time Struct:\t" )
+		  << Tag::Bright_Green( "[ " + std::to_string( sizeof( Cached_Date_Time ) ) + " bytes]\n" );
+
+	std::cout << Tag::Bright_Yellow( "Size of ColorConsole Class:\t\t" )
+		  << Tag::Bright_Green( "[ " + std::to_string( sizeof( ColorConsole ) ) + " bytes]\n" );
 
 	std::cout << Tag::Yellow( "***************************************************************\n" );
 	std::cout << Tag::Yellow( "***************************************************************\n" );
--- Serenity/include/serenity/Version.h
+++ Serenity/include/serenity/Version.h
@@ -2,7 +2,7 @@
 
 #define SERENITY_VERSION_MAJOR 0
 #define SERENITY_VERSION_MINOR 2
-#define SERENITY_VERSION_REVISION 41
+#define SERENITY_VERSION_REVISION 43
 
 #define VERSION_STRING_FORMAT( major, minor, revision ) #major "." #minor "." #revision
 #define VERSION_NUMBER( maj, min, rev )                 VERSION_STRING_FORMAT( maj, min, rev )
@@ -11,4 +11,4 @@
 #define SE_DAY             28
 #define SE_MONTH           10
 #define SE_YEAR            2021
-#define SE_LAST_BUILT   "11:59:54"
+#define SE_LAST_BUILT   "21:21:45"
