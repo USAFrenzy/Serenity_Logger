3351aafeb4c0d80ca54a9fb7cdf97f70116cc206
Ryan McCullough <mccullough.ryan123@gmail.com>
2022-01-12 03:42:32 -0500

    Fixed revision number tracking...

--- Serenity/include/serenity/Version.h
+++ Serenity/include/serenity/Version.h
@@ -1,14 +1,14 @@
 #pragma once
 
 #define SERENITY_VERSION_MAJOR 0
-#define SERENITY_VERSION_MINOR 0
-#define SERENITY_VERSION_REVISION 5
+#define SERENITY_VERSION_MINOR 2
+#define SERENITY_VERSION_REVISION 69
 
 #define VERSION_STRING_FORMAT( major, minor, revision ) #major "." #minor "." #revision
 #define VERSION_NUMBER( maj, min, rev )                 VERSION_STRING_FORMAT( maj, min, rev )
 
 // Last Built Statistics
-#define SE_DAY             10
+#define SE_DAY             12
 #define SE_MONTH           01
 #define SE_YEAR            2022
-#define SE_LAST_BUILT   "22:43:14"
+#define SE_LAST_BUILT   "03:41:57"
