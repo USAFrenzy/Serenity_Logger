9dad4a5abd3af3799843add8b99c62bbf99c8d85
Ryan McCullough <mccullough.ryan123@gmail.com>
2022-02-04 20:44:06 -0500

    Just refelcting Docs Dir Deletion From Original Branch

--- Serenity/include/serenity/Version.h
+++ Serenity/include/serenity/Version.h
@@ -2,13 +2,13 @@
 
 #define SERENITY_VERSION_MAJOR 0
 #define SERENITY_VERSION_MINOR 2
-#define SERENITY_VERSION_REVISION 94
+#define SERENITY_VERSION_REVISION 95
 
 #define VERSION_STRING_FORMAT( major, minor, revision ) #major "." #minor "." #revision
 #define VERSION_NUMBER( maj, min, rev )                 VERSION_STRING_FORMAT( maj, min, rev )
 
 // Last Built Statistics
-#define SE_DAY             03
+#define SE_DAY             04
 #define SE_MONTH           02
 #define SE_YEAR            2022
-#define SE_LAST_BUILT   "19:47:57"
+#define SE_LAST_BUILT   "20:43:17"
