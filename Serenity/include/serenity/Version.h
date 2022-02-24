4108a35cdf1fee1cdf5443232891a18950759fb0
Ryan <49297331+USAFrenzy@users.noreply.github.com>
2022-02-23 23:10:48 -0500

    Removed uneccessary caching for updates - not really sure what I had in mind with that when I originally wrote that bit, but I only ever used it when I couldn't update Version.h directly and had to rely on the incremental cache values

--- Serenity/include/serenity/Version.h
+++ Serenity/include/serenity/Version.h
@@ -2,7 +2,7 @@
 
 #define SERENITY_VERSION_MAJOR 0
 #define SERENITY_VERSION_MINOR 2
-#define SERENITY_VERSION_REVISION 111
+#define SERENITY_VERSION_REVISION 112
 
 #define VERSION_STRING_FORMAT( major, minor, revision ) #major "." #minor "." #revision
 #define VERSION_NUMBER( maj, min, rev )                 VERSION_STRING_FORMAT( maj, min, rev )
@@ -11,4 +11,4 @@
 #define SE_DAY         23
 #define SE_MONTH       02
 #define SE_YEAR        2022
-#define SE_LAST_BUILT  "23:00:05"
+#define SE_LAST_BUILT  "23:08:57"
--- cmake/Incremental_Build.cmake
+++ cmake/Incremental_Build.cmake
@@ -9,13 +9,6 @@ function(update_version_info ver_maj ver_min ver_rev)
     string(TIMESTAMP MONTH "%m")
     string(TIMESTAMP YEAR "%Y")
     string(TIMESTAMP LAST_TIME_COMPILED "%H:%M:%S")
-#Update the cache
-    file(WRITE "${CACHE_FILE}"
-    "Incremental Build Values:\n"
-    "Major: ${ver_maj}\n"
-    "Minor: ${ver_min}\n"
-    "Revision: ${ver_rev}\n"
-    )
 #Create, Or Update If Exists, The Version Header
     file(WRITE "${HEADER_FILE}" 
     "#pragma once\n\n"
@@ -34,10 +27,8 @@ function(update_version_info ver_maj ver_min ver_rev)
     )
 endfunction()
 
-# If The Cache File Exists, Read It In And Increment Revision Number.
-# If The Cache Doesn't Exist, But The Version.h File Does Exist,
-# (Example: Pulled From The Git Repo) Read Version.h In And Increment
-# Revision Number.
+# If The Version.h File Doesn't Exist, Create It; If It Does Exist,
+# Read Version.h In, Increment Revision Number, And Update Version.h.
 function(get_and_increment_version)
 
 if(EXISTS ${HEADER_FILE})
@@ -55,23 +46,6 @@ update_version_info(
     "${version_revision}"
 )
 set(SERENITY_FULL_VERSION "${version_major}.${version_minor}.${version_revision}" PARENT_SCOPE)
-   
-elseif(EXISTS ${CACHE_FILE})
-    file(READ "${CACHE_FILE}" CACHE_VALUE)
-    string(REGEX MATCH "Major: ([0-9]*)" _ "${CACHE_VALUE}")
-        set(version_major ${CMAKE_MATCH_1})
-    string(REGEX MATCH "Minor: ([0-9]*)" _ "${CACHE_VALUE}")
-        set(version_minor ${CMAKE_MATCH_1})
-    string(REGEX MATCH "Revision: ([0-9]*)" _ "${CACHE_VALUE}")
-        set(version_revision ${CMAKE_MATCH_1})
-    math(EXPR version_revision "${version_revision}+1")
-    update_version_info(
-        "${version_major}" 
-        "${version_minor}" 
-        "${version_revision}"
-    )
-    set(SERENITY_FULL_VERSION "${version_major}.${version_minor}.${version_revision}" PARENT_SCOPE)
-    
     else()
         set(version_major "0")
         set(version_minor "2")
