4ebd10e741a2832f3ac2c71a6c5b2d22b993632c
Ryan <mccullough.ryan123@gmail.com>
2022-03-07 19:37:13 -0500

    added MIT license and updated CMakeLists for what's being used in linux environment to start adding support there

--- Serenity/include/serenity/Version.h
+++ Serenity/include/serenity/Version.h
@@ -5,7 +5,7 @@
 
 #define SERENITY_VERSION_MAJOR 0
 #define SERENITY_VERSION_MINOR 2
-#define SERENITY_VERSION_REVISION 121
+#define SERENITY_VERSION_REVISION 123
 
 // clang-format on
 
@@ -13,7 +13,7 @@
 #define VERSION_NUMBER( maj, min, rev )                 VERSION_STRING_FORMAT( maj, min, rev )
 
 // Last Built Statistics
-#define SE_DAY         06
+#define SE_DAY         07
 #define SE_MONTH       03
 #define SE_YEAR        2022
-#define SE_LAST_BUILT  "23:45:12"
+#define SE_LAST_BUILT  "19:26:06"
