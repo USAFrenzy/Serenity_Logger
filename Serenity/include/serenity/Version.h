99673d0cc132c8d827beee0124d90a44bb5b2296
Ryan <49297331+USAFrenzy@users.noreply.github.com>
2022-05-06 09:15:01 -0400

    For real fixed appveyor.yml now; wasn't passing in the new build options in the command portion -> should pass now

--- Serenity/include/serenity/Version.h
+++ Serenity/include/serenity/Version.h
@@ -5,7 +5,7 @@
 
 #define SERENITY_VERSION_MAJOR 0
 #define SERENITY_VERSION_MINOR 2
-#define SERENITY_VERSION_REVISION 225
+#define SERENITY_VERSION_REVISION 227
 
 // clang-format on
 
@@ -16,4 +16,4 @@
 #define SE_DAY         06
 #define SE_MONTH       05
 #define SE_YEAR        2022
-#define SE_LAST_BUILT  "08:58:38"
+#define SE_LAST_BUILT  "09:11:51"
