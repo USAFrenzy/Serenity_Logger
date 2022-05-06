cec830166f53e1a215c894dc105e116ad86e3812
Ryan <49297331+USAFrenzy@users.noreply.github.com>
2022-05-06 09:04:00 -0400

    Forgot the '$' on two lines in CMakeLists.txt

--- Serenity/include/serenity/Version.h
+++ Serenity/include/serenity/Version.h
@@ -5,15 +5,15 @@
 
 #define SERENITY_VERSION_MAJOR 0
 #define SERENITY_VERSION_MINOR 2
-#define SERENITY_VERSION_REVISION 224
+#define SERENITY_VERSION_REVISION 225
 
 // clang-format on
 
-#define VERSION_STRING_FORMAT(major, minor, revision) #major "." #minor "." #revision
-#define VERSION_NUMBER(maj, min, rev)                 VERSION_STRING_FORMAT(maj, min, rev)
+#define VERSION_STRING_FORMAT( major, minor, revision ) #major "." #minor "." #revision
+#define VERSION_NUMBER( maj, min, rev )                 VERSION_STRING_FORMAT( maj, min, rev )
 
 // Last Built Statistics
-#define SE_DAY                                        06
-#define SE_MONTH                                      05
-#define SE_YEAR                                       2022
-#define SE_LAST_BUILT                                 "08:55:39"
+#define SE_DAY         06
+#define SE_MONTH       05
+#define SE_YEAR        2022
+#define SE_LAST_BUILT  "08:58:38"
